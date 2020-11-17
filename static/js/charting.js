3a3ac4e58b0b7ee1c49f97758e6898f0000ea60a
Jacky Wong <32987161+JackyFWong@users.noreply.github.com>
2020-11-16 22:19:42 -0500

    mvp 1

--- static/js/charting.js
+++ static/js/charting.js
@@ -1,11 +1,26 @@
 var chart_config = {
     type: 'line',
     data: {
-        datasets: [{
-            label: 'X',
-            fill: false,
-            borderColor: window.chartColors.red,
-        }]
+        datasets: [
+            {
+                label: 'X',
+                borderColor: 'rgba(255,0,0,0.8)',
+                backgroundColor: 'rgba(255,0,0,0.8)',
+                fill: false,
+            },
+            {
+                label: 'Y',
+                borderColor: 'rgba(0,255,0,0.8)',
+                backgroundColor: 'rgba(0,255,0,0.8)',
+                fill: false,
+            },
+            {
+                label: 'Z',
+                borderColor: 'rgba(0,0,255,0.8)',
+                backgroundColor: 'rgba(0,0,255,0.8)',
+                fill: false,
+            },
+        ]
     },
     options: {
         responsive: true,
@@ -35,14 +50,23 @@ var label = 0;
 
 window.onload = function() {
     var ctx = document.getElementById('canvas').getContext('2d');
-    window.testChart = new Chart(ctx, chart_config);
+    window.test_chart = new Chart(ctx, chart_config);
 }
 
 function add_data_to_chart(data) {
-    console.log(label);
+    if (data.sensors.gyro.x.length == 0) {
+        return;
+    }
     chart_config.data.labels.push(label++);
-    chart_config.data.datasets.forEach((dataset) => {
-        dataset.data.push(data.sensors.gyro.x[0]);
+    chart_config.data.datasets[0].data.push(data.sensors.gyro.x[0]);
+    chart_config.data.datasets[1].data.push(data.sensors.gyro.y[0]);
+    chart_config.data.datasets[2].data.push(data.sensors.gyro.z[0]);
+    window.test_chart.update();
+}
+
+function clear_chart() {
+    chart_config.data.labels = [];
+    chart_config.data.datasets.forEach( (dataset) => {
+        dataset.data = [];
     });
-    window.testChart.update();
 }
